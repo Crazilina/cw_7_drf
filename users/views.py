cc7fa53f27d4146f36ec1c2ee2a6dae30d3ce3a3
Alina Salakhova <alina.salakhova1991@gmail.com>
2024-07-14 00:19:42 +0200

    Добавлены декораторы swagger_auto_schema к методам UserViewSet (create, retrieve, update, partial_update, destroy) для улучшения API документации.

--- users/views.py
+++ users/views.py
@@ -2,6 +2,7 @@ from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from users.models import User
 from users.serializers import UserCreateSerializer, UserUpdateSerializer, UserProfileSerializer
+from drf_yasg.utils import swagger_auto_schema
 
 
 class UserViewSet(ModelViewSet):
@@ -13,11 +14,6 @@ class UserViewSet(ModelViewSet):
     def get_serializer_class(self):
         """
         Возвращает соответствующий сериализатор в зависимости от действия.
-
-        - Для действия 'create' возвращает UserCreateSerializer.
-        - Для действия 'retrieve' возвращает UserProfileSerializer.
-        - Для действий 'update' и 'partial_update' возвращает UserUpdateSerializer.
-        - Для всех остальных действий возвращает UserUpdateSerializer.
         """
         if self.action == 'create':
             return UserCreateSerializer
@@ -30,20 +26,52 @@ class UserViewSet(ModelViewSet):
     def get_permissions(self):
         """
         Возвращает соответствующие разрешения в зависимости от действия.
-
-        - Для действия 'create' возвращает AllowAny для разрешения создания пользователя без аутентификации.
-        - Для всех остальных действий возвращает IsAuthenticated для защиты действий аутентификацией.
         """
         if self.action == 'create':
             return [AllowAny()]
         return [IsAuthenticated()]
 
+    @swagger_auto_schema(
+        operation_description="Создание нового пользователя",
+        request_body=UserCreateSerializer,
+        responses={201: UserCreateSerializer}
+    )
+    def create(self, request, *args, **kwargs):
+        return super().create(request, *args, **kwargs)
+
+    @swagger_auto_schema(
+        operation_description="Получение данных пользователя по ID",
+        responses={200: UserProfileSerializer}
+    )
+    def retrieve(self, request, *args, **kwargs):
+        return super().retrieve(request, *args, **kwargs)
+
+    @swagger_auto_schema(
+        operation_description="Обновление данных пользователя по ID",
+        request_body=UserUpdateSerializer,
+        responses={200: UserUpdateSerializer}
+    )
+    def update(self, request, *args, **kwargs):
+        return super().update(request, *args, **kwargs)
+
+    @swagger_auto_schema(
+        operation_description="Частичное обновление данных пользователя по ID",
+        request_body=UserUpdateSerializer,
+        responses={200: UserUpdateSerializer}
+    )
+    def partial_update(self, request, *args, **kwargs):
+        return super().partial_update(request, *args, **kwargs)
+
+    @swagger_auto_schema(
+        operation_description="Удаление пользователя по ID",
+        responses={204: 'Нет содержимого'}
+    )
+    def destroy(self, request, *args, **kwargs):
+        return super().destroy(request, *args, **kwargs)
+
     def perform_create(self, serializer):
         """
         Переопределяет метод сохранения для установки пароля пользователя и активации учетной записи.
-
-        - Устанавливает пароль, используя set_password для сохранения пароля в зашифрованном виде.
-        - Устанавливает is_active в True для активации учетной записи.
         """
         user = serializer.save(is_active=True)
         user.set_password(serializer.validated_data['password'])
