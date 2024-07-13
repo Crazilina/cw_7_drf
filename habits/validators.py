97767b63f16ecbc8d683af54d12e8930aa979fef
Alina Salakhova <alina.salakhova1991@gmail.com>
2024-07-14 00:34:49 +0200

    Обновлены валидаторы для поддержки частичного обновления (PATCH) привычек

--- habits/validators.py
+++ habits/validators.py
@@ -1,5 +1,6 @@
 from rest_framework.serializers import ValidationError
 
+
 def validate_habit(data):
     """
     Валидация данных привычки.
@@ -12,17 +13,21 @@ def validate_habit(data):
         raise ValidationError("Нельзя указывать одновременно и вознаграждение, и связанную привычку.")
 
     # Время выполнения должно быть не больше 120 секунд
-    if data.get('execution_time') > 120:
+    execution_time = data.get('execution_time')
+    if execution_time is not None and execution_time > 120:
         raise ValidationError("Время на выполнение не может быть больше 120 секунд.")
 
     # В связанные привычки могут попадать только привычки с признаком приятной привычки
-    if data.get('related_habit') and not data['related_habit'].is_pleasant:
+    related_habit = data.get('related_habit')
+    if related_habit and not related_habit.is_pleasant:
         raise ValidationError("Связанная привычка должна быть приятной привычкой.")
 
     # У приятной привычки не может быть вознаграждения или связанной привычки
-    if data.get('is_pleasant') and (data.get('reward') or data.get('related_habit')):
+    is_pleasant = data.get('is_pleasant')
+    if is_pleasant and (data.get('reward') or related_habit):
         raise ValidationError("Приятная привычка не может иметь вознаграждение или связанную привычку.")
 
     # Нельзя выполнять привычку реже, чем 1 раз в 7 дней
-    if data.get('period') < 1 or data.get('period') > 7:
+    period = data.get('period')
+    if period is not None and (period < 1 or period > 7):
         raise ValidationError("Периодичность выполнения должна быть от 1 до 7 дней.")
